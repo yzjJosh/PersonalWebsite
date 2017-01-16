df334eb7b0a631e1d08611c304860a3ca655ca95
Josh <yangzijiangjosh@gmail.com>
2017-01-16 17:19:47 -0600

    Rename index controller to home controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
     #root page is 'index'
-    root to: 'index#index'
+    root to: 'home#show'
 
-    get 'index', to: 'index#index'
+    get 'home', to: 'home#show'
 
 
 end

5e2fdde4f17971c675418905ab7519f16e74cf4f
Josh <yangzijiangjosh@gmail.com>
2017-01-19 15:13:28 -0600

    Change view for job

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,9 @@
 class Person < ApplicationRecord
+    has_many :jobs, dependent: :destroy
+    has_many :educations, dependent: :destroy
+    has_many :honors, dependent: :destroy
+    has_many :publications, dependent: :destroy
+    has_many :interests, dependent: :destroy
+    has_many :skills, dependent: :destroy
+    has_many :languages, dependent: :destroy
 end

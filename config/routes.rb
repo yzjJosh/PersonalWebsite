e3999066255741d5707c649b9a1a6b481a9ed193
Josh <yangzijiangjosh@gmail.com>
2017-01-20 22:18:51 -0600

    Add ip filter on routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,25 @@
+class WhitelistConstraint
+  def initialize
+    # whitelist ip address for database access
+    @ips = ['127.0.0.1', '52.52.173.222'] 
+  end
+ 
+  def matches?(request)
+    @ips.include?(request.remote_ip)
+  end
+end
+
 Rails.application.routes.draw do
-  resources :people
-  resources :languages
-  resources :skills
-  resources :interests
-  resources :publications
-  resources :courses
-  resources :projects
-  resources :honors
-  resources :educations
-  resources :jobs
+    resources :people, :constraints => WhitelistConstraint.new
+    resources :languages, :constraints => WhitelistConstraint.new
+    resources :skills, :constraints => WhitelistConstraint.new
+    resources :interests, :constraints => WhitelistConstraint.new
+    resources :publications, :constraints => WhitelistConstraint.new
+    resources :courses, :constraints => WhitelistConstraint.new
+    resources :projects, :constraints => WhitelistConstraint.new
+    resources :honors, :constraints => WhitelistConstraint.new
+    resources :educations, :constraints => WhitelistConstraint.new
+    resources :jobs, :constraints => WhitelistConstraint.new
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
     #root page is 'index'

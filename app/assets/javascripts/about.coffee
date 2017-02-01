5f6642f2d52d9c35a5978c45df1cb0ae85e632b6
Josh <yangzijiangjosh@gmail.com>
2017-01-31 18:07:11 -0600

    Add education

--- app/assets/javascripts/about.coffee
+++ app/assets/javascripts/about.coffee
@@ -4,7 +4,23 @@
 
 $ -> 
     $('[data-toggle="popover"]').popover({      
-        trigger: 'hover'
+        trigger: 'hover',
+        html: true
     })
+
+    $('.expand-link').click((event) -> 
+        link_id = event.target.id
+        content_id = link_id.replace('-expand-', '-content-')
+        $("##{link_id}").attr('hidden', true)
+        $("##{content_id}").removeAttr('hidden')
+    )
+
+    $('.hide-link').click((event) -> 
+        id = event.target.id
+        link_id = id.replace('-hide-', '-expand-')
+        content_id = id.replace('-hide-', '-content-')
+        $("##{link_id}").removeAttr('hidden')
+        $("##{content_id}").attr('hidden', true)
+    )
 
 
--- app/helpers/about_helper.rb
+++ app/helpers/about_helper.rb
@@ -1,2 +1,48 @@
 module AboutHelper
+
+    def time(t)
+        t.strftime "%Y/%m/%d"
+    end
+
+    def time_range(start_t, end_t)
+        s = time(start_t)
+        if end_t == nil
+            e = 'Now'
+        else
+            e = time(end_t)
+        end
+        if s.eql?(e)
+            s
+        else
+            s + ' - ' + e
+        end
+    end
+
+    def gpa(gpa, scale)
+        if scale == nil
+            gpa.to_s
+        else
+            gpa.to_s + ' / ' + scale.to_s
+        end
+    end
+
+    def split_line(str)
+        str.split(/\n/)
+    end
+
+    def project_detail_tag(project)
+        description_tag = nil
+        if !project.description.blank?
+            description_tag = "<ul class='project-description-list'>"
+            project.description.split(/\n/).each do |line|
+                description_tag += "<li><p class='project-description'>#{line}</p></li>"
+            end
+            description_tag += "</ul>"
+        end
+        ("<div class='project-area'>\n" +
+         "  <p class='project-name'>#{project.name}</p>\n" +
+         "  <p class='gray project-time'>#{time_range project.start_time, project.end_time}</p>\n" +
+         "  #{description_tag}\n" +
+         "</div>").html_safe
+    end
 end

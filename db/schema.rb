6303d7aedcbac83db6f5fd548024ee4a94299769
Josh <yangzijiangjosh@gmail.com>
2019-01-19 12:43:30 -0800

    Rerun migrate to update schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,73 +13,73 @@
 ActiveRecord::Schema.define(version: 20170731075040) do
 
   create_table "courses", force: :cascade do |t|
-    t.string   "name",         limit: 255
+    t.string   "name"
     t.text     "description"
     t.integer  "education_id"
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-    t.index ["education_id"], name: "idx_courses_index_courses_on_education_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["education_id"], name: "index_courses_on_education_id"
   end
 
   create_table "educations", force: :cascade do |t|
-    t.string   "school",      limit: 255
-    t.string   "degree",      limit: 255
-    t.string   "location",    limit: 255
+    t.string   "school"
+    t.string   "degree"
+    t.string   "major"
+    t.string   "location"
     t.datetime "start_time"
     t.datetime "end_time"
-    t.decimal  "GPA",                     precision: 10, scale: 2
-    t.decimal  "GPA_scale",               precision: 10, scale: 2
-    t.string   "photo",       limit: 255
+    t.decimal  "GPA",         precision: 10, scale: 2
+    t.decimal  "GPA_scale",   precision: 10, scale: 2
+    t.string   "photo"
     t.text     "description"
     t.integer  "person_id"
-    t.datetime "created_at",                                       null: false
-    t.datetime "updated_at",                                       null: false
-    t.string   "major",       limit: 255
-    t.index ["person_id"], name: "idx_educations_index_educations_on_person_id"
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
+    t.index ["person_id"], name: "index_educations_on_person_id"
   end
 
   create_table "honors", force: :cascade do |t|
-    t.string   "name",        limit: 255
+    t.string   "name"
     t.datetime "time"
-    t.string   "granted_by",  limit: 255
-    t.string   "location",    limit: 255
+    t.string   "granted_by"
+    t.string   "location"
     t.text     "description"
     t.integer  "person_id"
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
-    t.index ["person_id"], name: "idx_honors_index_honors_on_person_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["person_id"], name: "index_honors_on_person_id"
   end
 
   create_table "interests", force: :cascade do |t|
-    t.string   "name",        limit: 255
+    t.string   "name"
     t.text     "description"
     t.integer  "person_id"
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
-    t.index ["person_id"], name: "idx_interests_index_interests_on_person_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["person_id"], name: "index_interests_on_person_id"
   end
 
   create_table "jobs", force: :cascade do |t|
-    t.string   "company",     limit: 255
-    t.string   "title",       limit: 255
-    t.string   "location",    limit: 255
+    t.string   "company"
+    t.string   "title"
+    t.string   "location"
     t.datetime "start_time"
     t.datetime "end_time"
-    t.string   "photo",       limit: 255
+    t.string   "photo"
     t.text     "description"
     t.integer  "person_id"
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
-    t.index ["person_id"], name: "idx_jobs_index_jobs_on_person_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["person_id"], name: "index_jobs_on_person_id"
   end
 
   create_table "languages", force: :cascade do |t|
-    t.string   "name",        limit: 255
-    t.string   "proficiency", limit: 255
+    t.string   "name"
+    t.string   "proficiency"
     t.integer  "person_id"
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
-    t.index ["person_id"], name: "idx_languages_index_languages_on_person_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["person_id"], name: "index_languages_on_person_id"
   end
 
   create_table "messages", force: :cascade do |t|
@@ -92,60 +92,60 @@ ActiveRecord::Schema.define(version: 20170731075040) do
   end
 
   create_table "people", force: :cascade do |t|
-    t.string   "name",              limit: 255
-    t.string   "profile_photo",     limit: 255
-    t.string   "title",             limit: 255
-    t.string   "company_or_school", limit: 255
-    t.string   "email",             limit: 255
-    t.string   "address",           limit: 255
-    t.string   "linkedin_url",      limit: 255
-    t.string   "github_url",        limit: 255
-    t.string   "facebook_url",      limit: 255
-    t.string   "google_plus_url",   limit: 255
-    t.string   "wechat_qrcode",     limit: 255
+    t.string   "name"
+    t.string   "profile_photo"
+    t.string   "title"
+    t.string   "company_or_school"
+    t.string   "email"
+    t.string   "address"
+    t.string   "linkedin_url"
+    t.string   "github_url"
+    t.string   "facebook_url"
+    t.string   "google_plus_url"
+    t.string   "wechat_qrcode"
     t.text     "description"
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
   end
 
   create_table "projects", force: :cascade do |t|
-    t.string   "name",         limit: 255
+    t.string   "name"
     t.datetime "start_time"
     t.datetime "end_time"
     t.text     "description"
     t.integer  "education_id"
     t.integer  "job_id"
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-    t.index ["education_id"], name: "idx_projects_index_projects_on_education_id"
-    t.index ["job_id"], name: "idx_projects_index_projects_on_job_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["education_id"], name: "index_projects_on_education_id"
+    t.index ["job_id"], name: "index_projects_on_job_id"
   end
 
   create_table "publications", force: :cascade do |t|
-    t.string   "name",        limit: 255
+    t.string   "name"
     t.datetime "time"
     t.text     "description"
     t.integer  "person_id"
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
-    t.index ["person_id"], name: "idx_publications_index_publications_on_person_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["person_id"], name: "index_publications_on_person_id"
   end
 
   create_table "skills", force: :cascade do |t|
-    t.string   "name",        limit: 255
+    t.string   "name"
     t.text     "description"
     t.integer  "person_id"
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
-    t.index ["person_id"], name: "idx_skills_index_skills_on_person_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["person_id"], name: "index_skills_on_person_id"
   end
 
   create_table "slides", force: :cascade do |t|
-    t.string   "title",      limit: 255
+    t.string   "title"
     t.text     "content"
-    t.string   "background", limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "background"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
